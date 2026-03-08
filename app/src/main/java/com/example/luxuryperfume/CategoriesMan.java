e72d972190ecf5dc3d078f515373c424e0e85080
nguyenhuudat337 <nguyenhuudat337@gmail.com>
2026-03-08 09:38:12 +0700

    day them categories

---
+++ app/src/main/java/com/example/luxuryperfume/CategoriesMan.java
@@ -0,0 +1,22 @@
+package com.example.luxuryperfume;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CategoriesMan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.categories_man);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.man), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/example/luxuryperfume/CategoriesSale.java
@@ -0,0 +1,26 @@
+package com.example.luxuryperfume;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class CategoriesSale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.categories_sale);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sale), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
